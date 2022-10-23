825d5d2f7b4c7ac0dd7b7267650625d6c1791f48
phatcharaphon23 <phatcharaphon.h@ku.th>
2022-10-23 16:04:52 +0700

    import_New

--- lib/Home/Page1.dart
+++ lib/Home/Page1.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_application_3/Class/Early_N_Def.dart';
+
 import 'package:flutter_application_3/Class/Healthyt_N.dart';
 import 'package:flutter_application_3/Class/Late_N_Def.dart';
 import 'package:flutter_application_3/Class/Prog_N_Def.dart';
-import 'package:flutter_application_3/Home/profile.dart';
-import 'package:flutter_application_3/Improve/Improve1.dart';
-import 'package:flutter_application_3/Thenme.dart';
+
 import 'package:image_picker/image_picker.dart';
-import 'package:http/http.dart' as http;
 
 import 'dart:io';
 
