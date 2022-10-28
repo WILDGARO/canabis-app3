69bc3d86c62818e7a4f1cd81c312a59cc7e8b1ec
WILDGARO <nawamin0322@gmail.com>
2022-10-28 16:41:29 +0700

    carePage

---
+++ lib/treatscreen/PerfectTreat.dart
@@ -0,0 +1,56 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class PerFectTreat extends StatefulWidget {
+  const PerFectTreat({super.key});
+
+  @override
+  State<PerFectTreat> createState() => _PerFectTreatState();
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+}
+
+class _PerFectTreatState extends State<PerFectTreat> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green[100],
+        title: Padding(
+          padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0),
+          child: Text('app.BAR').tr(),
+        ),
+      ),
+      body: EasyLocalization(
+        supportedLocales: [Locale('en'), Locale('th')],
+        path: 'assets/lang',
+        child: Container(
+          child: Align(
+            alignment: Alignment.center,
+            child: Padding(
+              padding: EdgeInsetsDirectional.fromSTEB(10, 10, 10, 10),
+              child: SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                child: Column(
+                  children: [
+                    Image.asset(
+                      "assets/qwe.jpg",
+                      scale: 5.0,
+                    ),
+                    Text('app.CANDATA').tr(),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/treatscreen/SickTreat.dart
@@ -0,0 +1,56 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class SickTreat extends StatefulWidget {
+  const SickTreat({super.key});
+
+  @override
+  State<SickTreat> createState() => _SickTreat();
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+}
+
+class _SickTreat extends State<SickTreat> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green[100],
+        title: Padding(
+          padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0),
+          child: Text('app.BAR').tr(),
+        ),
+      ),
+      body: EasyLocalization(
+        supportedLocales: [Locale('en'), Locale('th')],
+        path: 'assets/lang',
+        child: Container(
+          child: Align(
+            alignment: Alignment.center,
+            child: Padding(
+              padding: EdgeInsetsDirectional.fromSTEB(10, 10, 10, 10),
+              child: SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                child: Column(
+                  children: [
+                    Image.asset(
+                      "assets/qwe.jpg",
+                      scale: 5.0,
+                    ),
+                    Text('app.CANDATA').tr(),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
